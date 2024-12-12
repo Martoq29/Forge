eeb3675863aa8d15dbf32ca56a9bf511db1d47da
Martoq29 <148343004+Martoq29@users.noreply.github.com>
2024-12-13 00:50:07 +0100

    v4

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour
 {
     public Text scoreText;
+    public GameObject upgradesPanel; // Panel contenant les options d'amélioration
+
+    private bool upgradesVisible = false;
+
     public float currentScore;
     public float hitPower;
     public float scoreIncreasePerSecond;
     public float x;
-
 
     public int shop1prize;
     public Text shop1text;
@@ -36,10 +39,9 @@ public class Game : MonoBehaviour
     public GameObject plusObject;
     public Text plusText;
 
-    // Start is called before the first frame update
     void Start()
     {
-        currentScore =0;
+        currentScore = 0;
         hitPower = 1;
         scoreIncreasePerSecond = 1;
         x = 0f;
@@ -51,52 +53,60 @@ public class Game : MonoBehaviour
         amount2 = 0;
         amount2Profit = 5;
 
-        currentScore = PlayerPrefs.GetInt("currentScore", 0);
-        hitPower = PlayerPrefs.GetInt("hitPower", 1);
-        x = PlayerPrefs.GetInt("x", 0);
-
-        shop1prize = PlayerPrefs.GetInt("shop1prize", 25);
-        shop2prize = PlayerPrefs.GetInt("shop2prize", 125);
-        amount1 = PlayerPrefs.GetInt("amount1", 0);
-        amount1Profit = PlayerPrefs.GetInt("amount1Profit", 0);
-        amount2 = PlayerPrefs.GetInt("amount2", 0);
-        amount2Profit = PlayerPrefs.GetInt("amount2Profit", 0);
-        upgradePrize = PlayerPrefs.GetInt("upgradePrize", 50);
-
+        upgradePrize = 50;
         allUpgradePrize = 500;
-        
+
+        // Désactiver le panel des améliorations par défaut
+        upgradesPanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        scoreText.text = (int)currentScore + "$";
-        scoreIncreasePerSecond = x * Time.deltaTime;
-        currentScore = currentScore + scoreIncreasePerSecond;
+        // Formater uniquement le currentScore avec les suffixes
+        scoreText.text = FormatCurrentScore(currentScore) + "$";
 
+        scoreIncreasePerSecond = x * Time.deltaTime;
+        currentScore += scoreIncreasePerSecond;
+
+        // Afficher les autres valeurs sans suffixes
         shop1text.text = "Tier 1: " + shop1prize + " $";
         shop2text.text = "Tier 2: " + shop2prize + " $";
 
-        amount1Text.text = "Tier 1: "+amount1+" arts $: "+amount1Profit+"/s";
-        amount2Text.text = "Tier 2: " + amount2 + " arts $: " + amount2Profit + "/s";
+        amount1Text.text = "Tier 1: " + amount1 + "  anvil: " + amount1Profit + "/s";
+        amount2Text.text = "Tier 2: " + amount2 + "  anvil: " + amount2Profit + "/s";
 
-        upgradeText.text = "Cost: " + upgradePrize + " $";
+        if (upgradesVisible)
+        {
+            upgradeText.text = "Cost: " + upgradePrize + " $";
+            allUpgradeText.text = "Cost: " + allUpgradePrize + " $";
+        }
+    }
 
-        PlayerPrefs.SetInt("currentScore", (int)currentScore);
-        PlayerPrefs.SetInt("hitPower", (int)hitPower);
-        PlayerPrefs.SetInt("x", (int)x);
+    private string FormatCurrentScore(float number)
+    {
+        string[] suffixes = { "", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp" };
+        int magnitude = 0;
 
-        PlayerPrefs.SetInt("shop1prize", (int)shop1prize);
-        PlayerPrefs.SetInt("shop2prize", (int)shop2prize);
-        PlayerPrefs.SetInt("amount1", (int)amount1);
-        PlayerPrefs.SetInt("amount1Profit", (int)amount1Profit);
-        PlayerPrefs.SetInt("amount2", (int)amount2);
-        PlayerPrefs.SetInt("amount2Profit", (int)amount2Profit);
-        PlayerPrefs.SetInt("upgradePrize", (int)upgradePrize);
+        while (number >= 1000 && magnitude < suffixes.Length - 1)
+        {
+            number /= 1000;
+            magnitude++;
+        }
 
-        allUpgradeText.text = "Cost: " + allUpgradePrize + " $";
+        // Retourner le score formaté avec un suffixe et une seule décimale
+        return string.Format("{0:0.0}", number) + suffixes[magnitude];
+    }
 
-        plusText.text = "+ " + hitPower;
+    public void ShowUpgrades()
+    {
+        upgradesVisible = true;
+        upgradesPanel.SetActive(true);
+    }
+
+    public void CloseUpgrades()
+    {
+        upgradesVisible = false;
+        upgradesPanel.SetActive(false);
     }
 
     public void Hit()
@@ -123,8 +133,8 @@ public class Game : MonoBehaviour
             x += 1;
             shop1prize += 25;
         }
-        
     }
+
     public void Shop2()
     {
         if (currentScore >= shop2prize)
@@ -135,12 +145,11 @@ public class Game : MonoBehaviour
             x += 5;
             shop2prize += 125;
         }
-
     }
 
     public void Upgrade()
     {
-        if(currentScore >= upgradePrize)
+        if (currentScore >= upgradePrize)
         {
             currentScore -= upgradePrize;
             hitPower *= 2;
